b2de292d007bdee7a7febf61e7244442b9a52899
halilmert088 <ceylanmert86@gmail.com>
2022-08-31 15:28:36 +0300

    worked on implementing student DTO to student controller

--- src/main/java/com/example/demo/Controllers/DepartmentController.java
+++ src/main/java/com/example/demo/Controllers/DepartmentController.java
@@ -4,6 +4,9 @@ import java.util.*;
 
 import com.example.demo.Databases.Departments;
 import com.example.demo.Interfaces.departmentsInterface;
+import com.example.demo.DTO.*;
+import com.example.demo.Mappers.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/com/example/demo/Controllers/StudentController.java
+++ src/main/java/com/example/demo/Controllers/StudentController.java
@@ -2,8 +2,12 @@ package com.example.demo.Controllers;
 
 import com.example.demo.Databases.Students;
 import com.example.demo.Interfaces.studentsInterface;
+import com.example.demo.DTO.*;
+import com.example.demo.Mappers.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +18,19 @@ public class StudentController {
     @Autowired
     private studentsInterface studentsInterface;
 
-    @GetMapping("/showAll")
-    public List<Students> showAll(){
-        var users = (List<Students>) studentsInterface.findAll();
-        return users;
+    @Autowired private StudentMapper studentMapper;
+
+    @GetMapping("/showDTO")
+    public StudentDTO getStudent()
+    {
+        Students student = new Students();
+        student = studentsInterface.findAll().get(2); //test value, will work on getting parameter from input
+
+        StudentDTO studentDTO = new StudentDTO();
+        StudentDTO.StudentBuilder.studentBuilderWith().id(student.getId()).name(student.getName())
+                .surname(student.getSurname()).dept(DepartmentDTO.deptBuilder.deptBuilderWith()
+                        .id(student.getDept().getDeptId()).build()).build();
+
+        return studentDTO;
     }
 }
