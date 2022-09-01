0bb54fba380fa0fc66c36c8a0153176b95cb6102
halilmert088 <ceylanmert86@gmail.com>
2022-09-01 15:25:37 +0300

    DTO implementation almost complete, will further iron out the bugs on DTOs

--- src/main/java/com/example/demo/Controllers/DepartmentController.java
+++ src/main/java/com/example/demo/Controllers/DepartmentController.java
@@ -2,11 +2,13 @@ package com.example.demo.Controllers;
 
 import java.util.*;
 
+import com.example.demo.DTO.DepartmentDTO;
 import com.example.demo.Databases.Department;
 import com.example.demo.Interfaces.departmentsInterface;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +21,31 @@ public class DepartmentController {
     public List<Department> showDept(){
         var users = (List <Department>) deptInterface.findAll();
         return users;
+    }
+
+    @GetMapping("/showDeptDto")
+    public List<DepartmentDTO> showDTO()
+    {
+        List<DepartmentDTO> dept = new ArrayList<DepartmentDTO>();
+        List<Department> users = deptInterface.findAll();
+
+        for(int i = 0; i < users.size(); i++)
+        {
+            dept.add(DepartmentDTO.deptBuilder.deptBuilderWith().id(users.get(i).getDeptId())
+                    .name(users.get(i).getDeptName()).build());
+        }
+
+        return dept;
+    }
+
+    @GetMapping("/findDeptById")
+    public DepartmentDTO findById(@RequestParam(value = "id") int num)
+    {
+        Optional<Department> dept = deptInterface.findById(num);
+
+        DepartmentDTO dto = DepartmentDTO.deptBuilder.deptBuilderWith().id(dept.get().getDeptId()).
+                name(dept.get().getDeptName()).build();
+
+        return dto;
     }
 }
